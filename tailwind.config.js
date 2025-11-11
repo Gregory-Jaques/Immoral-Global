4f973254222eeed4fc18db8d21d9d61a1777fea7
Gregory-Jaques <gregoryjaques96@gmail.com>
2025-11-11 09:27:46 -0300

    Update: use local Tailwind output.css, remove CDN + style.css import; apply Lexend via font-sans; clean head links; add apps-script.md and imgs/line-box.png

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
     content: ["*"],
     theme: {
+        extend: {
+            fontFamily: {
+                'Lexend': ['Lexend', 'system-ui', 'sans-serif'],
+            }
+        }
     },
     plugins: [],
 }
